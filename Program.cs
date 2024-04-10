fa1abe5812ddb2f983eb1c771099226df86bc3ab
fffff1 <ildarsafianov@gmail.com>
2024-04-10 10:30:40 +0400

    l

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите знак");
+string zn = Console.ReadLine();
+
+int Sum = 0;
+
+if (zn =="+")
+{
+    Sum = A + B;
+}
+
+if (zn == "-")
+{
+    Sum = A - B;
+}
+
+if (zn == "*")
+{
+    Sum = A * B;
+}
+
+if (zn == "/")
+{
+    Sum = A / B;
+}
+
+Console.Write("Ответ: ");
+Console.Write(Sum);
